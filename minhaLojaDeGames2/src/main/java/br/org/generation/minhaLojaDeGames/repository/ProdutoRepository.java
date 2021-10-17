6d6f716ecb3c65c527a7cc9c65d3a9c7b30f8b4e
ALLANVILASBOAS <alan07vb@gmail.com>
2021-10-16 22:35:18 -0300

    up date lojaGames

--- minhaLojaDeGames2/src/main/java/br/org/generation/minhaLojaDeGames/repository/ProdutoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.org.generation.minhaLojaDeGames.repository;
-
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-import br.org.generation.minhaLojaDeGames.model.Produto;
-
-public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-	public List<Produto> findAllByTituloContainingIgnoreCase(String titulo);
-}
